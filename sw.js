1c4079a2377463e55ec2817bc0b7d4c017660cdd
gekoncaros <gekoncaros@gmail.com>
2026-09-14 07:52:22 -0700

    fix: refresh cached application shell

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wizytownik-v1';
+const CACHE='wizytownik-v2';
 const APP=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
