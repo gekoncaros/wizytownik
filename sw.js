cd178a20480c514ef485bc9fefdc31ea4b8ac4dd
gekoncaros <gekoncaros@gmail.com>
2026-09-14 07:45:20 -0700

    feat: add offline app shell

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='wizytownik-v1';
+const APP=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();if(new URL(e.request.url).origin===location.origin)caches.open(CACHE).then(c=>c.put(e.request,copy));return r;})));});
